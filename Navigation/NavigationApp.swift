b7c244a590e48754bf8be79729d71da599dcaf4d
Shruti Shivakumar <shivakumarshruti1@gmail.com>
2025-07-24 12:59:08 -0400

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Shruti Shivakumar on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
